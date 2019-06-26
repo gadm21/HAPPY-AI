677ea425343e48de1c80e52f7d41cd93479d7800
yeanwei97 <33476696+yeanwei97@users.noreply.github.com>
2019-06-26 17:13:35 +0800

    save error log in different folder

--- face-recognition-single-camera/src/autorun.py
+++ face-recognition-single-camera/src/autorun.py
@@ -11,7 +11,19 @@ try:
         # p = Popen("python3 " + filename, shell=True)    
         # timeout
         print('start')
-        p = Popen([sys.executable, filename])
+        now = datetime.datetime.now()
+        target = 'log/{}'.format(str(now)[:19])
+        if not os.path.exists(target):
+            os.mkdir(target)
+
+        outPath = '{}/out.log'.format(target)
+        errPath = '{}/err.log'.format(target)
+
+        p = Popen([sys.executable, filename, target], 
+            stdout=open(outPath, 'a+'), 
+            stderr=open(errPath, 'a+'),
+            )
+
         print('wait')
         p.wait()
         print('end')
